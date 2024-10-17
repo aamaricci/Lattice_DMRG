664019a7b4147fa8454476e39b9eb78877e932f9
Adriano Amaricci <adriano.amaricci@gmail.com>
2024-10-17 08:01:22 +0200

    Testing test

--- src/VERSION.f90
+++ src/VERSION.f90
@@ -1,6 +1,7 @@
 MODULE VERSION
   implicit none
 <<<<<<< HEAD
+<<<<<<< HEAD
   !GIT VERSION
 <<<<<<< HEAD
   character(len=41),parameter :: git_code_version = "9fd873586968c311b4be609a7c2aeb1970886932"	
@@ -10,4 +11,8 @@ MODULE VERSION
 =======
   character(len=41),parameter :: git_code_version = "7e90d6adb23b00b52f6d1c0419371a5091f3b743"	
 >>>>>>> 82ec5df (Included local test for each auxiliary module in their respective directories.)
+=======
+  !GIT VERSION
+  character(len=41),parameter :: git_code_version = "aa4dff1a4ebd51d4dfa912ed8d9bee5bba8fc943"	
+>>>>>>> b9bb505 (Testing test)
 END MODULE VERSION
