a8ed1a60d008a02693e3f13820c8f006a15b4f96
Adriano Amaricci <adriano.amaricci@gmail.com>
2025-08-18 08:48:41 +0200

    Implemented newer algorithm for MVP used in ARPACK.
    The term A_q.x.B_q in H_superblock was treated as:
    
    (A_q.x.B_q).v -> C_q.v = Hv
    
    now:
    
    (A_q.x.B_q).v -> (A_q.x.B_q)vec(V_q) -> vec(B_q.V_q.A^t_q) = vec(Hv)

--- src/VERSION.f90
+++ src/VERSION.f90
@@ -1,5 +1,5 @@
 MODULE VERSION
   implicit none
   !GIT VERSION
-  character(len=41),parameter :: git_code_version = "77e1b958ccb22498e453f47b1c4b33794fabf5b6"	
+  character(len=41),parameter :: git_code_version = "fd1accdfa32e80b0a27821426a127dd1f81ad922"	
 END MODULE VERSION
